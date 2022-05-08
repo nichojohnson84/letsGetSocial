ffb2f4133736ba54147da2569a1d4687690affaf
Nick Johnson <nickcjohnson84@outlook.com>
2022-05-07 23:47:15 -0600

    user controller almost complete

--- controllers/user.js
+++ controllers/user.js
@@ -59,6 +59,59 @@ const userController = {
 
   //delete User
   //deleteUser({ params }), res) {
-    
+  //   User.findOneAndDelete({ _id: params.id })
+  //     .then(dbUserData => res.json(dbUserData))
+  //     .catch(err => res.json(err));
+  // },
+  
+  //Delete user and users associated thoughts
+  deleteUser({ params }, res) {
+    Thought.deleteMany({ userId: params.id })
+      .then(() => {
+        User.findOneAndDelete({ userId: params.id})
+          .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No User found with this ID homie!' });
+              return;
+            }
+            res.json(dbUserData);
+          });
+      })
+      .catch(err => res.json(err));
+  },
+
+  // /api/users/:userid/friends/:friendId
+  addFriend({ params} res) {
+    User.findOneAndUpdate(
+      {_id: params.userId },
+      { $push: {friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No User found with this Id boss!'});
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  deleteFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No User found with this Id boss!'});
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
   }
 };
+
+module.exports = userController
