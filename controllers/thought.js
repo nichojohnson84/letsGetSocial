631593bdb4a00e473ddb0777f3a78632e256bc18
Nick Johnson <nickcjohnson84@outlook.com>
2022-05-11 18:57:35 -0600

    thought controller syntax

--- controllers/thought.js
+++ controllers/thought.js
@@ -1,16 +1,13 @@
 const { User, Thought } = require('../models');
 
 const thoughtController = {
-  // /api/thoughts
-
-  // get all thoughts
   getAllThought(req, res) {
     Thought.find({})
-      .populate({
+      /*.populate({
         path: 'reactions',
         select: '-__v',
       })
-      .select('-__v')
+      .select('-__v')*/
       .sort({ _id: -1 })
       .then((dbThoughtData) => res.json(dbThoughtData))
       .catch((err) => {
